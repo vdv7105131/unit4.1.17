1f025b13c049e755bab3d9d0e8ee55ee13c4a119
vdv7105131 <vdv7105131@gmail.com>
2021-10-11 09:08:43 +0300

    add unit4.1.17

---
+++ unit4.1.17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+		var color = Console.ReadLine();
+
+		if (color == "red")
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is red!");
+		}
+
+		else if (color == "green")
+		{
+			Console.BackgroundColor = ConsoleColor.W;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is green!");
+		}
+		else
+		{
+			Console.BackgroundColor = ConsoleColor.Cyan;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is cyan!");
+		}
+	}
+}
